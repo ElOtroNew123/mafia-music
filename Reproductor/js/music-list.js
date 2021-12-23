f8a5385c972c584e6635d483ff0258ac84ee687b
ElOtroNew123 <91492097+ElOtroNew123@users.noreply.github.com>
2021-12-23 18:20:23 -0500

    Add files via upload

---
+++ Reproductor/js/music-list.js
@@ -0,0 +1,86 @@
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+
+  {
+    name: "Toxic",
+    artist: "BoyWithUke",
+    img: "music-7",
+    src: "music-7",
+  },
+
+  {
+    name: "On & On (feat. Daniel Levi)",
+    artist: "Cartoon",
+    img: "music-10",
+    src: "music-8",
+  },
+  {
+    name: "Elektronomia",
+    artist: "Vision",
+    img: "music-8",
+    src: "music-9",
+  },
+  {
+    name: "Fearless",
+    artist: "Pink Floyd",
+    img: "music-9",
+    src: "music-10",
+  },
+  {
+    name: "Heroes Tonight",
+    artist: "Janji",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "POPSICLE",
+    artist: "UHSN",
+    img: "music-12",
+    src: "music-12",
+  },
+  {
+    name: "Duro 2 Horas",
+    artist: "Faraon Love Shady X Tame Impala",
+    img: "music-13",
+    src: "music-13",
+  },
+
+
+  
+];
